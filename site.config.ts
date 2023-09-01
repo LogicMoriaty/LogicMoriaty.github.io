076cbefa5f067260a149e4b4a197fc812f82fd41
LogicMoriaty <1430104514@qq.com>
2023-09-01 09:00:37 +0800

    try

--- site.config.ts
+++ site.config.ts
@@ -3,94 +3,97 @@ import { defineSiteConfig } from 'valaxy'
 export default defineSiteConfig({
   url: 'https://valaxy.site/',
   lang: 'zh-CN',
-  title: 'Valaxy Theme Yun',
+  title: "-Lomori's blog~-",
+  subtitle:"~~~~~~~~~~~~~~~~",
+  description: 'Per Aspera, Ad Astra——',
   author: {
-    name: '云游君',
+    name: 'Logic·Moriaty',
   },
-  description: 'Valaxy Theme Yun Preview.',
+  //description: 'Valaxy Theme Yun Preview.',
   social: [
     {
       name: 'RSS',
-      link: '/atom.xml',
+      //link: '/atom.xml',
       icon: 'i-ri-rss-line',
       color: 'orange',
     },
     {
       name: 'QQ 群 1050458482',
-      link: 'https://qm.qq.com/cgi-bin/qm/qr?k=kZJzggTTCf4SpvEQ8lXWoi5ZjhAx0ILZ&jump_from=webapi',
+      //link: 'https://qm.qq.com/cgi-bin/qm/qr?k=kZJzggTTCf4SpvEQ8lXWoi5ZjhAx0ILZ&jump_from=webapi',
       icon: 'i-ri-qq-line',
       color: '#12B7F5',
     },
     {
       name: 'GitHub',
-      link: 'https://github.com/YunYouJun',
+      link: 'https://github.com/LogicMoriaty',
       icon: 'i-ri-github-line',
       color: '#6e5494',
     },
     {
       name: '微博',
-      link: 'https://weibo.com/jizhideyunyoujun',
+      //link: 'https://weibo.com/jizhideyunyoujun',
       icon: 'i-ri-weibo-line',
       color: '#E6162D',
     },
     {
       name: '豆瓣',
-      link: 'https://www.douban.com/people/yunyoujun/',
+      //link: 'https://www.douban.com/people/yunyoujun/',
       icon: 'i-ri-douban-line',
       color: '#007722',
     },
     {
       name: '网易云音乐',
-      link: 'https://music.163.com/#/user/home?id=247102977',
+      link: 'https://music.163.com/#/user/home?id=307368691',
       icon: 'i-ri-netease-cloud-music-line',
       color: '#C20C0C',
     },
     {
       name: '知乎',
-      link: 'https://www.zhihu.com/people/yunyoujun/',
+      link: 'https://www.zhihu.com/people/logicmoriaty',
       icon: 'i-ri-zhihu-line',
       color: '#0084FF',
     },
     {
       name: '哔哩哔哩',
-      link: 'https://space.bilibili.com/1579790',
+      link: 'https://space.bilibili.com/119842720',
       icon: 'i-ri-bilibili-line',
       color: '#FF8EB3',
     },
     {
       name: '微信公众号',
-      link: 'https://cdn.yunyoujun.cn/img/about/white-qrcode-and-search.jpg',
+      //link: 'https://cdn.yunyoujun.cn/img/about/white-qrcode-and-search.jpg',
       icon: 'i-ri-wechat-2-line',
       color: '#1AAD19',
     },
     {
       name: 'Twitter',
-      link: 'https://twitter.com/YunYouJun',
+      //link: 'https://twitter.com/YunYouJun',
       icon: 'i-ri-twitter-line',
       color: '#1da1f2',
     },
     {
       name: 'Telegram Channel',
-      link: 'https://t.me/elpsycn',
+      //link: 'https://t.me/elpsycn',
       icon: 'i-ri-telegram-line',
       color: '#0088CC',
     },
     {
       name: 'E-Mail',
-      link: 'mailto:me@yunyoujun.cn',
+      //link: 'mailto:me@yunyoujun.cn',
       icon: 'i-ri-mail-line',
       color: '#8E71C1',
     },
     {
       name: 'Travelling',
-      link: 'https://www.travellings.cn/go.html',
+      //link: 'https://www.travellings.cn/go.html',
       icon: 'i-ri-train-line',
       color: 'var(--va-c-text)',
     },
   ],
 
   search: {
-    enable: false,
+    enable: true,
+    type: 'fuse',	//"algolia"或"engine"或"fuse"，代码有注释
   },
 
   sponsor: {
@@ -99,19 +102,19 @@ export default defineSiteConfig({
     methods: [
       {
         name: '支付宝',
-        url: 'https://cdn.yunyoujun.cn/img/donate/alipay-qrcode.jpg',
+        //url: 'https://cdn.yunyoujun.cn/img/donate/alipay-qrcode.jpg',
         color: '#00A3EE',
         icon: 'i-ri-alipay-line',
       },
       {
         name: 'QQ 支付',
-        url: 'https://cdn.yunyoujun.cn/img/donate/qqpay-qrcode.png',
+        //url: 'https://cdn.yunyoujun.cn/img/donate/qqpay-qrcode.png',
         color: '#12B7F5',
         icon: 'i-ri-qq-line',
       },
       {
         name: '微信支付',
-        url: 'https://cdn.yunyoujun.cn/img/donate/wechatpay-qrcode.jpg',
+        //url: 'https://cdn.yunyoujun.cn/img/donate/wechatpay-qrcode.jpg',
         color: '#2DC100',
         icon: 'i-ri-wechat-pay-line',
       },
