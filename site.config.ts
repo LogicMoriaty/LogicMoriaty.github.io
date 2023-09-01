9b321d625d326e7ec1899207d850a0e3f3a06755
LogicMoriaty <1430104514@qq.com>
2023-09-01 17:11:22 +0800

    try

--- site.config.ts
+++ site.config.ts
@@ -17,30 +17,30 @@ export default defineSiteConfig({
       icon: 'i-ri-rss-line',
       color: 'orange',
     },
-    {
+    /* {
       name: 'QQ 群 1050458482',
       //link: 'https://qm.qq.com/cgi-bin/qm/qr?k=kZJzggTTCf4SpvEQ8lXWoi5ZjhAx0ILZ&jump_from=webapi',
       icon: 'i-ri-qq-line',
       color: '#12B7F5',
-    },
+    }, */
     {
       name: 'GitHub',
       link: 'https://github.com/LogicMoriaty',
       icon: 'i-ri-github-line',
       color: '#6e5494',
     },
-    {
+    /* {
       name: '微博',
       //link: 'https://weibo.com/jizhideyunyoujun',
       icon: 'i-ri-weibo-line',
       color: '#E6162D',
-    },
-    {
+    }, */
+    /* {
       name: '豆瓣',
       //link: 'https://www.douban.com/people/yunyoujun/',
       icon: 'i-ri-douban-line',
       color: '#007722',
-    },
+    }, */
     {
       name: '网易云音乐',
       link: 'https://music.163.com/#/user/home?id=307368691',
@@ -59,36 +59,36 @@ export default defineSiteConfig({
       icon: 'i-ri-bilibili-line',
       color: '#FF8EB3',
     },
-    {
+    /* {
       name: '微信公众号',
       //link: 'https://cdn.yunyoujun.cn/img/about/white-qrcode-and-search.jpg',
       icon: 'i-ri-wechat-2-line',
       color: '#1AAD19',
-    },
-    {
+    }, */
+    /* {
       name: 'Twitter',
       //link: 'https://twitter.com/YunYouJun',
       icon: 'i-ri-twitter-line',
       color: '#1da1f2',
-    },
-    {
+    }, */
+    /* {
       name: 'Telegram Channel',
       //link: 'https://t.me/elpsycn',
       icon: 'i-ri-telegram-line',
       color: '#0088CC',
-    },
-    {
+    }, */
+    /* {
       name: 'E-Mail',
       //link: 'mailto:me@yunyoujun.cn',
       icon: 'i-ri-mail-line',
       color: '#8E71C1',
-    },
-    {
+    }, */
+    /* {
       name: 'Travelling',
       //link: 'https://www.travellings.cn/go.html',
       icon: 'i-ri-train-line',
       color: 'var(--va-c-text)',
-    },
+    }, */
   ],
 
   search: {
--- valaxy.config.ts
+++ valaxy.config.ts
@@ -26,6 +26,36 @@ export default defineValaxyConfig<UserThemeConfig>({
       },
     },
 
+    // 主题颜色
+    colors: {
+      primary: "#141dce", 
+    },
+
+    // 鼠标点击特效
+    fireworks: {
+      enable: true,
+      //colors: ['#fef7f1', '#fed1d8', '#bdd8d6']
+    },
+
+    // 网页背景
+    bg_image: {
+      enable: true,
+      url: "https://bu.dusays.com/2021/10/03/c77ab5169ae7f.png",	// 白日模式背景
+      dark: "https://bu.dusays.com/2023/05/08/6458b04446a4f.jpg",	// 夜间模式背景
+    //   这个是透明度
+      opacity: 0.6,
+    },
+
+    // 首页中二语句（嘿嘿，这个是搬运另一个博主的）
+    say: {
+      enable: true,
+      api: "https://el-bot-api.vercel.app/api/words/young",
+      hitokoto: {
+        enable: true,
+        api: "https://v1.hitokoto.cn/?c=k&c=d&c=i",
+      },
+    },
+
     pages: [
       /* {
         name: '我的小伙伴们',
@@ -37,7 +67,7 @@ export default defineValaxyConfig<UserThemeConfig>({
       {
         name: '留言板',
         url: '/comments/',
-        icon: 'ri-clipboard-line',
+        icon: 'i-ri-clipboard-line',
         color: '#4321ee',
       },
       {
@@ -71,4 +101,26 @@ export default defineValaxyConfig<UserThemeConfig>({
 
 
   unocss: { safelist },
+
+  /* // 评论插件功能，插件想要对应添加
+  addons: [
+  addonWaline({
+    serverURL: "写入对应的URL",		// Waline服务链接
+    locale: {
+      placeholder: "填写qq邮箱或点击登录，可以展示个人头像",
+    },
+    comment: true,
+    pageview: true,
+    emoji: [
+      "https://jsd.onmicrosoft.cn/gh/walinejs/emojis@latest/bmoji/",
+      "https://jsd.onmicrosoft.cn/npm/@waline/emojis@latest/weibo/",
+      "https://jsd.onmicrosoft.cn/npm/@waline/emojis@latest/qq/",
+      "https://jsd.onmicrosoft.cn/npm/@waline/emojis@latest/tieba/",
+    ],
+  }),
+  // 注意注意，这个是相册功能
+  addonLightGallery(),
+], */
+
+
 })
