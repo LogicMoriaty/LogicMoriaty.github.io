b2d10099e358f1a7c126276ed2fe184ed005dae4
LogicMoriaty <1430104514@qq.com>
2024-08-05 10:56:53 +0800

    update

--- vue.d.ts
+++ vue.d.ts
@@ -3,3 +3,11 @@ declare module '*.vue' {
   const Component: ReturnType<typeof defineComponent>
   export default Component
 }
+
+
+// declare module '*.vue' {
+//   import type { DefineComponent } from 'vue'
+//      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+//   const component: DefineComponent<{}, {}, any>
+//   export default component
+// }
