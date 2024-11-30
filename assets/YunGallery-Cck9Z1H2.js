a10a88143a4176aaa3744bb3f8261ec300ed089a
LogicMoriaty <LogicMoriaty@users.noreply.github.com>
2024-11-30 01:55:42 +0000

    deploy: af8e5239b309552b66353afeda6439b1edde7880

---
+++ assets/YunGallery-Cck9Z1H2.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-DSKokqsz.js";import"./app-D6WkQutu.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-0E8y6U8p.js";export{o as default};
---
+++ assets/post-C3wn0G72.js
@@ -0,0 +1,1 @@
+import{a3 as c,E as i}from"./app-D6WkQutu.js";function u(o){if(!o)return{color:"",icon:"",styles:void 0};const r=c();o in r.value.types||(o="link");const s=r.value.types[o].color,n=r.value.types[o].icon,e=i(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:n,styles:e}}export{u};
